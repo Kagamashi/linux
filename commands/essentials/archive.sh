eb62b25ffd3a713307a0cea1a75343e658437d95
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-12-05 14:27:14 +0100

    Modified: commands/essentials/archive.sh

--- commands/essentials/archive.sh
+++ commands/essentials/archive.sh
@@ -10,39 +10,27 @@ unzip                                   # Extract ZIP archives
 # Packs files and directories into a single .tar file (also called tarball)
 # Tar Archive also stores permission and ownership informations about files and directories
 
-tar --list --file archive.tar # Display context of an archive
-tar -tf archive.tar
-
-x --extract
-c --create
-f --file
-r --append
-t --list
-
 z --gzip
 j --bzip2
 J --xz
 
 tar -cvf archive.tar /path/to/directory     # create a tarball (archive) from a directory
 tar -rf archive.tar file2                   # add file to archived directory
+tar -tf archive.tar.gz                      # view contents of tar file
 tar -xvf archive.tar                        # extract files from a tarball
 tar -czvf archive.tar.gz /path/to/directory # create a compressed tarball using gzip
 tar -xzvf archive.tar.gz                    # extract a compressed tarball
 tar xf archive.tar -C /tmp                  # extract in another directory
 
-# To ensure all permission/ownership informations get restored
-sudo tar --extract --file archive.tar --directory /tmp/
+sudo tar --extract --file archive.tar --directory /tmp/ # to ensure all permission/ownership informations get restored
 
-# gzip – Compress files using gzip
 gzip file.txt                             # compress file.txt to file.txt.gz
 gzip -d file.txt.gz                       # decompress file.txt.gz to file.txt
 gunzip file1.gz
 
-# bzip2 – Compress files using bzip2
 bzip2 file.txt                            # compress file.txt to file.txt.bz2
 bzip2 -d file.txt.bz2                     # decompress file.txt.bz2 to file.txt
 bunzip file2.bz2
 
-# zip – Create ZIP archives
 zip archive.zip file1.txt file2.txt      # create a ZIP archive containing file1.txt and file2.txt
 unzip archive.zip                        # extract the contents of archive.zip
