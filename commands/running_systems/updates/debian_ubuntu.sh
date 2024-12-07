98225154f4be18b7bbfaecd8b257e0181ab835df
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-12-07 11:30:55 +0100

    Modified: commands/running_systems/updates/debian_ubuntu.sh

--- commands/running_systems/updates/debian_ubuntu.sh
+++ commands/running_systems/updates/debian_ubuntu.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
 
 apt                                      # High-level package manager
 apt-get                                  # Low-level package handling utility
 dpkg                                     # Debian package manager
 
-# apt – High-level package manager
 apt update                               # Update the package list
 apt upgrade                              # Upgrade all installed packages
 apt install <package_name>               # Install a package
@@ -12,7 +10,6 @@ apt remove <package_name>                # Remove a package
 apt search <search_term>                 # Search for a package
 apt show <package_name>                  # Show details about a package
 
-# apt-get – Low-level package handling utility
 apt-get update                           # Update the package list
 apt-get upgrade                          # Upgrade all installed packages
 apt-get install <package_name>           # Install a package
@@ -20,7 +17,6 @@ apt-get remove <package_name>            # Remove a package
 apt-get autoremove                       # Remove unnecessary packages
 apt-get -f install                       # Fix broken dependencies
 
-# dpkg – Debian package manager
 dpkg -i <package_file.deb>              # Install a .deb package file
 dpkg -r <package_name>                   # Remove a package
 dpkg -l                                  # List all installed packages
