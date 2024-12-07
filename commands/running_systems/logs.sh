36002dd9ff2157885a4c4c960036bdf4c9fd74e7
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-12-07 11:30:55 +0100

    Modified: commands/running_systems/logs.sh

--- commands/running_systems/logs.sh
+++ commands/running_systems/logs.sh
@@ -1,42 +1,27 @@
 
-# Logs collected inside /var/log directory
-grep -r 'ssh' /var/log/ # To find in what files ssh logs are stored
-tail -F /var/log/secure # To see new log entries as soon as they appear (debugging)
+grep -r 'ssh' /var/log/                   # to find in what files ssh logs are stored
+grep "error" /var/log/syslog              # search for the term "error" in syslog
+grep -i "warning" /var/log/syslog         # search for "warning" (case insensitive)
+grep -r "fatal" /path/to/directory        # recursively search for "fatal" in files in a directory
+grep -n "timeout" filename.txt            # search for "timeout" and show line numbers
 
-last # Who logged last into the system
-lastlog # Shows all users and last time they loggen in
+last        # who logged last into the system
+lastlog     # shows all users and last time they logged in
 
-systemctl status                        # Check the status of a service
-journalctl                              # Query the systemd journal (logs)
+systemctl status                        # check the status of a service
 
-# journalctl – Query the systemd journal (logs)
-journalctl                              # Show all logs
-journalctl -u <service_name>            # Show logs for a specific service
-journalctl --since "2 hours ago"        # Show logs since a specific time
-journalctl -f                           # Follow logs in real time (similar to tail -f)
-journalctl --list-boots                 # List all boots and their corresponding log identifiers
-journalctl -e # Open journal and go to the end of the output
-journalctl -p # Show list of priority options: alert, crit, debug, emerg, err, info, notice, warning
-journalctl -p info -g '^b' # g is for grep
-journalctl -S 01:00 -U 02:00 # Logs recorded since specified time
-journalctl -S '2024-10-10 10:10:55' # Logs recorded on specific date and time
-journalctl -b 0 # Logs from current boot
-journalctl -b 1 # Logs from previous boot
+journalctl                              # show all logs
+journalctl -u <service_name>            # show logs for a specific service
+journalctl --since "2 hours ago"        # show logs since a specific time
+journalctl -f                           # follow logs in real time (similar to tail -f)
+journalctl --list-boots                 # list all boots and their corresponding log identifiers
+journalctl -e                           # open journal and go to the end of the output
+journalctl -p                           # show list of priority options: alert, crit, debug, emerg, err, info, notice, warning
+journalctl -p info -g '^b'              # g is for grep
+journalctl -S 01:00 -U 02:00            # logs recorded since specified time
+journalctl -S '2024-10-10 10:10:55'     # logs recorded on specific date and time
+journalctl -b 0                         # logs from current boot
+journalctl -b 1                         # logs from previous boot
 
-which sudo
-journalctl /bin/sudo # View logs of specific command
-
-#############
-
-tail -f /var/log/syslog                 # Monitor logs in real time
-grep                                      # Search logs or files for errors and issues
-
-# tail -f – Monitor logs in real time
-tail -f /var/log/syslog                  # Continuously display new log entries in syslog
-tail -f /var/log/syslog | grep error      # Monitor syslog for lines containing "error"
-
-# grep – Search logs or files for errors and issues
-grep "error" /var/log/syslog             # Search for the term "error" in syslog
-grep -i "warning" /var/log/syslog         # Search for "warning" (case insensitive)
-grep -r "fatal" /path/to/directory        # Recursively search for "fatal" in files in a directory
-grep -n "timeout" filename.txt            # Search for "timeout" and show line numbers
+tail -f /var/log/syslog                  # display log entries in real time
+tail -f /var/log/syslog | grep error     # monitor syslog for lines containing "error"
