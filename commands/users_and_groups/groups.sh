8fbaedb1367bc44b0d02a84f7df8441e7be30007
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-12-07 13:12:26 +0100

    Modified: commands/users_and_groups/groups.sh

--- commands/users_and_groups/groups.sh
+++ commands/users_and_groups/groups.sh
@@ -1,28 +1,22 @@
-#!/bin/bash
 
-groupadd                                # Add a new group
-groupmod                                # Modify group attributes
-groupdel                                # Delete a group
-groups                                  # Shows the groups the current user belongs to.
-gpasswd                                 # AddS OR Remove user to/from a group
-newgrp                                  # Switch to a new group (in an active session)
+groupadd                                # add a new group
+groupmod                                # modify group attributes
+groupdel                                # delete a group
+groups                                  # shows the groups the current user belongs to
+gpasswd                                 # add OR remove user to/from a group
+newgrp                                  # switch to a new group (in an active session)
 
-# groupadd – Add a new group
-groupadd newgroup                       # Create a new group named "newgroup"
-groupadd -g 1001 newgroup # Creates group with specific GID (Group ID)
+groupadd newgroup                       # create a new group named "newgroup"
+groupadd -g 1001 newgroup               # creates group with specific GID (Group ID)
 
-# groupmod – Modify group attributes
-groupmod -n newgroupname oldgroupname  # Rename a group from "oldgroupname" to "newgroupname"
+groupmod -n newgroupname oldgroupname   # rename a group from "oldgroupname" to "newgroupname"
 groupmod -g 2000 groupname
 
-# groupdel – Delete a group
-groupdel oldgroupname                   # Delete the group "oldgroupname"
+groupdel oldgroupname                   # delete the group "oldgroupname"
 
-# groups
-groups $user # Shows groups the specified user is part
+groups $user                            # shows groups the specified user is part of
 
-gpasswd -d username group_name          # Remove a user from a specified group.
-gpasswd -a username group_name          # Add a user to a group using `gpasswd`.
-gpasswd -d username group_name          # Remove a user from a group using `gpasswd`.
+gpasswd -d username group_name          # remove user from a specified group
+gpasswd -a username group_name          # add user to a group using
 
-newgrp group_name                            # Temporarily switch to another group for the current session.
+newgrp group_name                       # temporarily switch to another group for the current session
