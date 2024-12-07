a4e9cee1f72334c8617bf2aa62f7a4b26a90fc5f
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-12-07 13:12:26 +0100

    Modified: commands/users_and_groups/resource_limits.sh

--- commands/users_and_groups/resource_limits.sh
+++ commands/users_and_groups/resource_limits.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
 
-ulimit # built-in shell command to display and set resource limits for the current shell session.
+ulimit    # built-in shell command to display and set resource limits for the current shell session
 
-
-ulimit -a                                 # Display all resource limits for the current user session.
-ulimit -n                                 # Maximum number of open file descriptors.
-ulimit -u                                 # Maximum number of processes a user can create.
-ulimit -n 2048                            # Set the soft limit for the maximum number of open files (2048).
-ulimit -u 100                             # Set the soft limit for the maximum number of processes (100).
-ulimit -Hn 4096                           # Set the hard limit for open files to 4096.
-ulimit -Hu 200                            # Set the hard limit for the number of processes to 200.
+ulimit -a                    # display all resource limits for the current user session
+ulimit -n                    # maximum number of open file descriptors
+ulimit -u                    # maximum number of processes a user can create
+ulimit -n 2048               # set the soft limit for the maximum number of open files (2048)
+ulimit -u 100                # set the soft limit for the maximum number of processes (100)
+ulimit -Hn 4096              # set the hard limit for open files to 4096
+ulimit -Hu 200               # set the hard limit for the number of processes to 200
