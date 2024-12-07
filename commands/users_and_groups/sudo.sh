6c5ac16f03ad791745d687c0e4ca4b4d67e207cb
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-12-07 13:12:26 +0100

    Modified: commands/users_and_groups/sudo.sh

--- commands/users_and_groups/sudo.sh
+++ commands/users_and_groups/sudo.sh
@@ -1,13 +1,9 @@
-# Grant temporary superuser privileges
-sudo command                               # Run a command with superuser (root) privileges.
-sudo su                                    # Switch to the root user (superuser).
 
-# Show which privileges a user has
-sudo -l                                    # List the sudo privileges for the current user.
-sudo -l -U username                        # List the sudo privileges for a specific user.
+sudo command                               # run a command with superuser (root) privileges
+sudo su                                    # switch to the root user (superuser)
+sudo -l                                    # list the sudo privileges for the current user
+sudo -l -U username                        # list the sudo privileges for a specific user
 
-# Add a user to the sudoers list
-sudo usermod -aG sudo username             # Add a user to the "sudo" group, granting them sudo privileges.
+sudo usermod -aG sudo username             # add a user to sudoers group, granting them sudo privileges
 
-# Open the sudoers file for editing:
-sudo visudo
+sudo visudo                                # open sudo file for editing
