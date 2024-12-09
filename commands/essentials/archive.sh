176c65a502c63271d42e69c905ea23fc7ccfb8c1
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-12-09 15:38:59 +0100

    Modified: commands/essentials/archive.sh

--- commands/essentials/archive.sh
+++ commands/essentials/archive.sh
@@ -22,7 +22,8 @@ tar -czvf archive.tar.gz /path/to/directory # create a compressed tarball using 
 tar -xzvf archive.tar.gz                    # extract a compressed tarball
 tar xf archive.tar -C /tmp                  # extract in another directory
 
-sudo tar --extract --file archive.tar --directory /tmp/ # to ensure all permission/ownership informations get restored
+tar X<(ls | file -f - | grep -i MPEG | cut -d: -f 1) -c -f file.tar * # exclude MPEG file from content of file.tar
+sudo tar --extract --file archive.tar --directory /tmp/               # to ensure all permission/ownership informations get restored
 
 gzip file.txt                             # compress file.txt to file.txt.gz
 gzip -d file.txt.gz                       # decompress file.txt.gz to file.txt
@@ -34,3 +35,13 @@ bunzip file2.bz2
 
 zip archive.zip file1.txt file2.txt      # create a ZIP archive containing file1.txt and file2.txt
 unzip archive.zip                        # extract the contents of archive.zip
+
+
+## BACKUP
+dd if=/dev/sda of=/system_images/sda.img # backup a device (must be unmounted)
+dd if=/system_images/sda.img of=/dev/sda # restore device
+
+rsync   # keep content of two directories synchronized
+rsync -av source dest                                       # synchronize source with dest -a archive, provide a series of default option
+rsync -avz /tmp user@123.123.123.123:/dest                  # synchronize tmp with dest that it's contained in remote machine with IP 123.123.123.123 (-z means code will be compressed during transfer)
+rsync -avzhe ssh source root@remote_host:/remote_directory/ # synchronize source with remote_directory using ssh
