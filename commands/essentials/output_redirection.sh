059867d90651c123b381f0e27980ecba2d8aa0e1
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-12-09 15:38:59 +0100

    Modified: commands/essentials/output_redirection.sh

--- commands/essentials/output_redirection.sh
+++ commands/essentials/output_redirection.sh
@@ -1,16 +1,18 @@
-stdin  # Standard input: receives input (usually from the keyboard or a file)
-stdout # Standard output: sends normal output (usually to the screen)
-stderr # Standard error: sends error messages (usually to the screen)
+stdin  # standard input: receives input (usually from the keyboard or a file)
+stdout # standard output: sends normal output (usually to the screen)
+stderr # standard error: sends error messages (usually to the screen)
 
-< file                              # Redirect input from "file"
+< file                              # redirect input from "file"
 
-> file                              # Create/overwrite a file
-1> file                             # Redirect standard output (stdout) to "file" (overwrite)
+> file                              # create/overwrite a file
+1> file                             # redirect standard output (stdout) to "file" (overwrite)
 
->> file                             # Create/append to a file instead of overwriting
-1>> file                            # Redirect stdout and append to "file"
+>> file                             # create/append to a file instead of overwriting
+1>> file                            # redirect stdout and append to "file"
 
-2> errors.txt                       # Redirects stderr (error messages) to "errors.txt"
+2> errors.txt                       # redirects stderr (error messages) to "errors.txt"
+2>&1                                # redirect stderr to same place as stdout
+|                                   # 'pipe' - use output of the previous command as input in the next one
 
 # /dev/null is often used to get rid of unnecessary output
 grep -r 'pattern' /etc/ 2>/dev/null  # display normal output, suppress errors
