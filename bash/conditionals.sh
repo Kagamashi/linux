bd4c32b4ad2c00117516abaf41066da2078b1e60
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-12-18 14:31:42 +0100

    Modified: bash/conditionals.sh

--- bash/conditionals.sh
+++ bash/conditionals.sh
@@ -1,6 +1,5 @@
 
-# if, elif, else
-
+# IF / ELIF / ELSE
 # if [ condition ]; then
 #     commands
 # elif [ condition ]; then
@@ -88,3 +87,47 @@ fi
 if [ -e "$file" ]; then
     echo "$file exists"
 fi
+
+
+# FOR : looping over lists
+# for var in list; do commands; done
+for fruit in apple banana cherry; do
+    echo "I like $fruit"
+done
+
+# FOR : looping over a range of numbers
+# for var in {start..end}; do commands; done
+for num in {1..5}; do
+    echo "Number: $num"
+done
+
+# WHILE
+# while [ condition ]; do commands; done
+counter=1
+while [ $counter -le 5 ]; do
+    echo "Counter is at $counter"
+    ((counter++))
+done
+
+# UNTIL
+# until [ condition ]; do commands; done
+counter=1
+until [ $counter -gt 5 ]; do
+    echo "Until loop, counter is at $counter"
+    ((counter++))
+done
+
+# BREAK/CONTINUE
+for num in {1..10}; do
+    if [ $num -eq 5 ]; then
+        echo "Breaking at $num"
+        break
+    fi
+
+    if [ $num -eq 3 ]; then
+        echo "Skipping number $num"
+        continue
+    fi
+
+    echo "Number: $num"
+done
