7a20a1956c1cfb349f1badaeb65ae517fbf7229a
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-12-18 12:00:41 +0100

    Modified: bash/arrays.sh

--- bash/arrays.sh
+++ bash/arrays.sh
@@ -1,27 +1,24 @@
-#!/bin/bash
 
-# Declaring an array
+# declaring an array
 fruits=("apple" "banana" "cherry" "date")
 
-# Accessing array elements
+# accessing array elements
 echo "First element: ${fruits[0]}"
 echo "Second element: ${fruits[1]}"
 
-# Iterating over arrays
+# iterating over arrays
 for fruit in "${fruits[@]}"; do
     echo "I like $fruit"
 done
 
-# Array length
+# array length
 length=${#fruits[@]}
 echo "The array contains $length elements"
 
-# Array slicing
-# Syntax: ${array[@]:start:length}
-# Start at index 1 and get 2 elements
-sliced_fruits=("${fruits[@]:1:2}")
+# array slicing
+# ${array[@]:start:length}
+sliced_fruits=("${fruits[@]:1:2}")      # start at index 1 and get 2 elements
 echo "Sliced array (from index 1, length 2): ${sliced_fruits[@]}"
 
-# Another example: starting from the second element and getting the remaining ones
-remaining_fruits=("${fruits[@]:2}")
+remaining_fruits=("${fruits[@]:2}")     # starting from the second element and getting the remaining ones
 echo "Remaining fruits from index 2: ${remaining_fruits[@]}"
