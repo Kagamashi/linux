5d2cdfd84dacb75d999901312b7834b1f9bbb262
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-12-19 09:58:08 +0100

    Modified: bash/variables.sh

--- bash/variables.sh
+++ bash/variables.sh
@@ -1,20 +1,17 @@
-#!/bin/bash
 
-# 1. Declaring Variables
-# Variables in Bash are assigned without spaces between the variable name, the equals sign, and the value.
+# declaration
 my_var="Hello, World"
 
-# 2. Environment Variables
-# Environment variables are available to sub-processes and can be exported to make them global.
-export MY_ENV_VAR="I am an environment variable"
+# environment variables
+export MY_ENV_VAR="I am an environment variable" # exported env variables are available globally
 
-# 3. Special Variables
-$0  # The name of the script itself
-$1, $2, ... # Positional parameters representing command-line arguments
-$@  # All positional parameters as a list
-$#  # The number of positional parameters
-$$  # The process ID of the script
-$?  # The exit status of the last executed command
+# special variables
+$0          # name of the script itself
+$1, $2, ... # positional parameters representing command-line arguments
+$@          # all positional parameters as a list
+$#          # number of positional parameters
+$$          # process ID of the script
+$?          # exit status of the last executed command
 
 echo "Script name: $0"
 echo "First argument: $1"
@@ -23,12 +20,7 @@ echo "Number of arguments: $#"
 echo "Process ID: $$"
 echo "Exit status of last command: $?"
 
-# 4. Using export for Environment Variables
-# When you want to make a variable available globally (to child processes), use export.
-export GLOBAL_VAR="This is a global variable"
-echo "Global variable: $GLOBAL_VAR"
-
-# 5. Variable Substitution
+# variable substitution
 # ${var} - Basic variable substitution
 # ${var:-default} - If the variable is not set, use the default value
 # ${var:=default} - If the variable is not set, assign it the default value
@@ -38,13 +30,20 @@ echo "Variable substitution: ${my_var:-'Default value'}"
 my_var=${my_var:='Default value assigned'}
 echo "Variable after assignment: $my_var"
 
-# 6. Arrays in Bash
-# Declare an array with parentheses and access elements with index notation.
+# arrays
 my_array=("apple" "banana" "cherry")
 echo "First element: ${my_array[0]}"
 echo "All elements: ${my_array[@]}"
 echo "Number of elements in array: ${#my_array[@]}"
 
-# Adding an element to an array
+# add element to array
 my_array[3]="date"
 echo "Added element: ${my_array[3]}"
+
+# shell expansions
+echo file{A,B,C}.txt  # creates fileA.txt, fileB.txt, fileC.txt
+echo {1..5}           # expands to 1 2 3 4 5
+echo {a..d}           # expands to a b c d
+
+# tilde expansion: ~
+# expands to the home directory of the current user
