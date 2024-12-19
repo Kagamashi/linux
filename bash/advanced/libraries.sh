ef294df9c17f2576f9e68caaf54fd6cc02413613
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-12-19 10:35:27 +0100

    Modified: bash/advanced/libraries.sh

--- bash/advanced/libraries.sh
+++ bash/advanced/libraries.sh
@@ -1,32 +1,24 @@
-#!/bin/bash
 
-# A simple function that prints a greeting
+# ./my_library.sh
 greet() {
     echo "Hello, $1!"
 }
 
-# A function that calculates the sum of two numbers
 sum() {
     local num1=$1
     local num2=$2
     echo "The sum of $num1 and $num2 is $((num1 + num2))"
 }
 
-# A function that displays the current date
 display_date() {
     echo "Today's date is: $(date)"
 }
 
+###
 
-### Use library in another script .sh file (MAIN script)
-# Source the library file to use its functions
-source ./my_library.sh
-
-# Call the greet function from the library
+# ./main.sh
+# use library in another script .sh file (MAIN script)
+source ./my_library.sh  # source library file to use its functions
 greet "Alice"
-
-# Call the sum function from the library
 sum 5 10
-
-# Call the display_date function from the library
 display_date
