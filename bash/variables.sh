146119caee3862e4fdcb146587b94b74b9b69c82
Mikolaj <mikolaj.borecki1@gmail.com>
2025-01-25 17:55:17 +0100

    Modified: bash/variables.sh

--- bash/variables.sh
+++ bash/variables.sh
@@ -14,37 +14,19 @@ $#          # number of positional parameters
 $$          # process ID of the script
 $?          # exit status of the last executed command
 
-echo "Script name: $0"
-echo "First argument: $1"
-echo "All arguments: $@"
-echo "Number of arguments: $#"
-echo "Process ID: $$"
-echo "Exit status of last command: $?"
-
 # variable substitution
-# ${var} - Basic variable substitution
-# ${var:-default} - If the variable is not set, use the default value
-# ${var:=default} - If the variable is not set, assign it the default value
+${var}          # Basic variable substitution
+${var:-default} # If the variable is not set, use the default value
+${var:=default} # If the variable is not set, assign it the default value
 
-unset my_var # Unset the variable to demonstrate default substitution
-echo "Variable substitution: ${my_var:-'Default value'}"
-my_var=${my_var:='Default value assigned'}
+unset my_var    # removes variable from the environment making it undefined
+echo "Variable substitution: ${my_var:-'Default value'}" # my_var remains unset
+my_var=${my_var:='Default value assigned'}  # assign default value to my_var if unset
 echo "Variable after assignment: $my_var"
-
-# arrays
-my_array=("apple" "banana" "cherry")
-echo "First element: ${my_array[0]}"
-echo "All elements: ${my_array[@]}"
-echo "Number of elements in array: ${#my_array[@]}"
-
-# add element to array
-my_array[3]="date"
-echo "Added element: ${my_array[3]}"
 
 # shell expansions
 echo file{A,B,C}.txt  # creates fileA.txt, fileB.txt, fileC.txt
 echo {1..5}           # expands to 1 2 3 4 5
 echo {a..d}           # expands to a b c d
 
-# tilde expansion: ~
-# expands to the home directory of the current user
+~   # tilde expansion : expands to the home directory of the current user
