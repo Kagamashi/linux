2c89c7d736afb251ff4e9f58e4cb4b5bda74c34e
Mikolaj <mikolaj.borecki1@gmail.com>
2025-01-25 17:55:15 +0100

    Modified: bash/advanced/getopts.sh

--- bash/advanced/getopts.sh
+++ bash/advanced/getopts.sh
@@ -18,23 +18,23 @@ AGE=""
 # parsing command-line options using getopts
 while getopts "hn:a:v" opt; do
     case $opt in
-        h)
+        h)  # Handle the help option
+            usage                              # Call the usage function
+            exit 0                             # Exit successfully
+            ;;
+        n)  # Handle the name option
+            NAME=$OPTARG                      # Assign the provided name to the NAME variable
+            ;;
+        a)  # Handle the age option
+            AGE=$OPTARG                       # Assign the provided age to the AGE variable
+            ;;
+        v)  # Handle the verbose option
+            VERBOSE=1                         # Enable verbose mode by setting VERBOSE to 1
+            ;;
+        \?) # Handle invalid options
+            echo "Invalid option: -$OPTARG" >&2  # Print an error message for invalid options
             usage
-            exit 0
-            ;;
-        n)
-            NAME=$OPTARG
-            ;;
-        a)
-            AGE=$OPTARG
-            ;;
-        v)
-            VERBOSE=1
-            ;;
-        \?)
-            echo "Invalid option: -$OPTARG" >&2
-            usage
-            exit 1
+            exit 1                               # Exit with an error
             ;;
     esac
 done
