d67467c5c852d450edca257b24db9fa22fffa067
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-14 17:31:15 +0100

    Added new file: 03_shell_and_scripting/

---
+++ 03_shell_and_scripting/shell_tools/tail.sh
@@ -0,0 +1,8 @@
+
+# tail command prints the last N lines of a file (default: 10 lines).
+
+tail file.txt            # Show last 10 lines
+tail -n 20 file.txt      # Show last 20 lines
+tail -f logfile.log      # Continuously monitor a log file
+
+# Tip: Use tail -f for real-time log monitoring.
