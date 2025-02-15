8634b49a87f252552753b8909e2f3c0577ae9aae
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-15 10:51:53 +0100

    Modified: 06_networking/network_services_and_troubleshooting/ping.sh

---
+++ 06_networking/network_services_and_troubleshooting/ping.sh
@@ -0,0 +1,8 @@
+
+
+ping example.com                         # send ICMP echo requests to example.com
+ping -c 4 192.168.1.1                    # send 4 pings to a specific IP address
+
+ping example.com                        # send ICMP echo requests to example.com
+ping -c 4 192.168.1.1                   # send 4 pings to a specific IP address
+ping -i 0.5 example.com                 # ping with a 0.5-second interval between requests
