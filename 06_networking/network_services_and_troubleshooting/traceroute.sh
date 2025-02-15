b8a82d6e8b552cfb2832e65984343b2463d364e9
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-15 10:59:10 +0100

    Modified: 06_networking/network_services_and_troubleshooting/traceroute.sh

--- 06_networking/network_services_and_troubleshooting/traceroute.sh
+++ 06_networking/network_services_and_troubleshooting/traceroute.sh
@@ -1,8 +1,8 @@
 
-# print the route packets take to the network host
+# traceroute shows the path packets take to reach a destination
+# on some systems use 'mtr example.com' for continuous traceroute
 
-traceroute example.com                   # trace the route to example.com
-traceroute -n 192.168.1.1                # trace the route without resolving hostnames
-
-traceroute example.com                  # trace the route packets take to example.com
-traceroute -m 15 192.168.1.1            # limit the max number of hops to 15
+traceroute example.com           # trace the route to example.com
+traceroute -n 192.168.1.1        # trace the route without resolving hostnames
+traceroute -m 15 192.168.1.1     # limit the max number of hops to 15
+traceroute -I example.com        # use ICMP instead of UDP
