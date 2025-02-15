eef7bcf9906e96ff8b9fe76719aa4e88e4cd8c9f
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-15 11:50:59 +0100

    Modified: 03_shell_and_scripting/shell_tools/tail.sh

--- 03_shell_and_scripting/shell_tools/tail.sh
+++ 03_shell_and_scripting/shell_tools/tail.sh
@@ -6,3 +6,5 @@ tail -n 20 file.txt      # Show last 20 lines
 tail -f logfile.log      # Continuously monitor a log file
 
 # Tip: Use tail -f for real-time log monitoring.
+tail -f /var/log/syslog  # Follow system logs live
+tail -n 100 /var/log/syslog  # Display the last 100 lines 
