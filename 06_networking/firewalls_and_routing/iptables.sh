ff67415a0ba7cf1345c7308c6f9758fe21dc9cc3
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-15 11:07:46 +0100

    Modified: 06_networking/firewalls_and_routing/iptables.sh

--- 06_networking/firewalls_and_routing/iptables.sh
+++ 06_networking/firewalls_and_routing/iptables.sh
@@ -1,17 +1,15 @@
 
-# administer IP packet filter rules
+# [legacy firewall management]
+# iptables is a rule-based firewall used for packet filtering
 
-iptables -L                                   # list all current iptables rules
-iptables -A INPUT -s 192.168.1.100 -j ACCEPT  # allow incoming traffic from a specific IP
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT # allow incoming SSH connections
-iptables -F                                   # flush all rules (use with caution)
+sudo iptables -L -v                                 # list firewall rules (IPv4)
+iptables -F                                         # flush all rules (use with caution)
 
-
-## Firewall Configuration
-# iptables
-sudo iptables -L                                    # list firewall rules (IPv4)
 sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT  # allow SSH on port 22
-sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT  # allow HTTP on port 80
+sudo iptables -A INPUT -p tcp --dport 80 -j DROP    # block HTTP on port 80
 sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT # allow HTTPS on port 443
 sudo iptables -A INPUT -j DROP                      # block all other connections
-sudo iptables-save                                  # save the firewall rules
+
+sudo iptables-save                  # save the firewall rules
+sudo iptables-save > rules.v4
+sudo iptables-restore < rules.v4    # restore firewall rules
